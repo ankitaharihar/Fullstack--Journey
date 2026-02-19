ee5458fa4ce7af63b34fa626420733e9d0bc749d
Ankita <ankitaharihar81@gmail.com>
2026-02-19 12:29:47 +0530

    Add Day 4 content: Implement greater than and less than operators with examples in JavaScript

--- frontend/javascript/day 4/script.js
+++ frontend/javascript/day 4/script.js
@@ -2,3 +2,8 @@
 console.log(5 > 3); //true
 //less than operator
 console.log(5 < 3); //false
+
+
+
+let age = 25;
+age>18 ? console.log("You are an adult") : console.log("You are a minor"); //You are an adult
