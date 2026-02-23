5d66632ce1050da9ba8eb93b691c21c0e1240ec6
Ankita <ankitaharihar81@gmail.com>
2026-02-23 12:30:05 +0530

    Refactor Day 6 content: Remove old index.html and script.js files, and add new index.html with loop examples and updated script.js for FizzBuzz implementation

---
+++ frontend/javascript/day 6/script.js
@@ -0,0 +1,34 @@
+//for(let num = 1;num<=10;num++)
+//    {console.log("Hii", num)
+        
+ //   }
+
+// print 5 to 15
+//for (let num=5;num<=15;num++){
+//    console.log(num)
+//}
+// print 0 to -15
+//for (let num=0;num>=-15;num--){
+  //  console.log(num)
+//}
+
+//for (let num=0;num<=20;num++){
+ //   if (num%2==0){
+  //  console.log(num)
+//}
+//}
+
+
+
+for (num=1; num<=100;num++){
+    if (num%3==0 &&num%5==0){
+         console.log("fizzbuzz");
+    }else if (num%3==0){
+        console.log("fizz");
+    }else if (num%5==0){
+        console.log("Buzz");
+    }else {
+        console.log(num);
+    }
+
+    }
