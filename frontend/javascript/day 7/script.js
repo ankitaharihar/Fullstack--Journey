e1c8c9a4621b3eccae800c5c9bede6e471883000
Ankita <ankitaharihar81@gmail.com>
2026-02-25 11:38:58 +0530

    Add index.html and script.js for Day 7 content: Include predefined and user-defined functions

---
+++ frontend/javascript/day 7/script.js
@@ -0,0 +1,23 @@
+// alert will return a undefined value
+let c = alert("Good Morning")
+console.log(c)
+
+// confirm will return a boolean value true or false based on the user input
+let a = confirm("Are you sure?")
+if(a){
+    console.log("User is sure")
+}else{
+    console.log("User is not sure")
+}
+
+
+
+console.log(a)  
+
+// prompt will return a string value based on the user input
+let b = prompt("Enter your name")
+console.log(b)
+
+
+
+
