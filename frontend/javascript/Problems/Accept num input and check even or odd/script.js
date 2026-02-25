fb46b2eb6988108482ffc88ca4ae9f0a2ddd2a65
Ankita <ankitaharihar81@gmail.com>
2026-02-25 20:13:09 +0530

    Add HTML and JavaScript files for Even or Odd number checking implementation

---
+++ frontend/javascript/Problems/Accept num input and check even or odd/script.js
@@ -0,0 +1,11 @@
+let num = prompt("Enter num")
+
+
+
+if(num % 2 ==0){
+    console.log(`${num} is an even Number`)
+}else{
+    console.log(`${num} is an odd Number`)
+}
+
+
--- frontend/javascript/day 7/script.js
+++ frontend/javascript/day 7/script.js
@@ -1,22 +1,24 @@
-// alert will return a undefined value
-let c = alert("Good Morning")
-console.log(c)
-
-// confirm will return a boolean value true or false based on the user input
-let a = confirm("Are you sure?")
-if(a){
-    console.log("User is sure")
-}else{
-    console.log("User is not sure")
-}
+// // alert will return a undefined value
+// let c = alert("Good Morning")
+// console.log(c)
 
 
 
-console.log(a)  
+// // confirm will return a boolean value true or false based on the user input
+// let a = confirm("Are you sure?")
+// if(a){
+//     console.log("User is sure")
+// }else{
+//     console.log("User is not sure")
+// }
+// console.log(a)  
+
+
+
 
 // prompt will return a string value based on the user input
-let b = prompt("Enter your name")
-console.log(b)
+let a = prompt("Enter your name")
+console.log(a)
 
 
 
