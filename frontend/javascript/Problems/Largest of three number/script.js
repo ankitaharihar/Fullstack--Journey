b0cbaeaf1c32b7c92a7b0cfb7f9e5733355f9af6
Ankita <ankitaharihar81@gmail.com>
2026-02-25 15:37:46 +0530

    Add implementation for Largest of Three Numbers with HTML and JavaScript files

---
+++ frontend/javascript/Problems/Largest of three number/script.js
@@ -0,0 +1,11 @@
+let num1 = 4;
+let num2 = 8;
+let num3 = 8.5;
+
+if(num1>=num2 && num1>=num3 ){
+    console.log(num1  +  "is largest number")
+}else if (num2>=num3){
+    console.log(num2  +  "is largest number")
+} else{
+    console.log(num3  +  "is largest number")
+}
