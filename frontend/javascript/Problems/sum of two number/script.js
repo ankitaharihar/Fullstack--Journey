9ffa4dae30963fb4f4d7afcb8fb186e38c5efe05
Ankita <ankitaharihar81@gmail.com>
2026-02-25 14:59:17 +0530

    Refactor: Remove old problem files and add new implementations for prime number checking, simple calculator, and sum of two numbers

---
+++ frontend/javascript/Problems/sum of two number/script.js
@@ -0,0 +1,18 @@
+// sum of two number
+
+// let num1 = 6;
+// let num2 = 9;
+
+// let sum = num1 + num2;
+   
+// console.log(sum);
+
+//sum of 1 to 10 number
+let sum = 0
+
+for(let i=1;i<=10;i++){
+   
+    sum = sum + i
+} 
+
+console.log(sum)
