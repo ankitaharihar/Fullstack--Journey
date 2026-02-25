70b2cc87e6528963c32e900c72c5a9b33629ba63
Ankita <ankitaharihar81@gmail.com>
2026-02-25 09:08:37 +0530

    Add Simple Calculator implementation with HTML and JavaScript files

---
+++ frontend/javascript/Problems/problem2/script.js
@@ -0,0 +1,44 @@
+let num1 = 5;
+let num2 = 5;
+let op = "-"
+let result;
+
+if (op === "+"){
+    result = num1 + num2;
+}else if (op === "-"){
+    result = num1 - num2;
+}else if (op === "*"){
+    result = num1 * num2;
+}else {
+  console.log("Invalid op");
+}
+
+if(result !== undefined ){
+    console.log(result);
+}
+
+
+
+
+// Function 
+function cal(a,b,op){
+    let result;
+    if (op === "+") {
+    result = a + b;
+}
+else if (op === "-") {
+  result = a - b;
+}
+else if (op === "*") {
+  result = a * b;
+}
+else if (op === "/") {
+  result = a / b;
+}
+else {
+  console.log("invalid")
+}
+return result;
+}
+
+console.log(cal(3,4,"+"))
