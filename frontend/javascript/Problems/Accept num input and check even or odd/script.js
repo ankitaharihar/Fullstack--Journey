74a9a285644461384828bac379c34459fb5d1b91
Ankita <ankitaharihar81@gmail.com>
2026-02-25 21:31:45 +0530

    Add implementation for sum of two numbers with HTML and JavaScript files

--- frontend/javascript/Problems/Accept num input and check even or odd/script.js
+++ frontend/javascript/Problems/Accept num input and check even or odd/script.js
@@ -1,7 +1,4 @@
 let num = prompt("Enter num")
-
-
-
 if(num % 2 ==0){
     console.log(`${num} is an even Number`)
 }else{
---
+++ frontend/javascript/Problems/Take Two num ip and display sum/script.js
@@ -0,0 +1,6 @@
+let num1 = Number(prompt("Enter num1"));
+let num2 = Number(prompt("Enter num2"));
+
+let sum = num1 + num2 ;
+
+console.log(sum);
