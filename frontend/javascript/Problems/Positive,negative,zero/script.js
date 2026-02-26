a86d31ba9b88731501ce04b8cf47a02a68780567
Ankita <ankitaharihar81@gmail.com>
2026-02-26 08:16:49 +0530

    Add implementation for Positive, Negative, Zero checker with HTML and JavaScript files

---
+++ frontend/javascript/Problems/Positive,negative,zero/script.js
@@ -0,0 +1,10 @@
+function posiNegaZero(a){
+    if(a>0){
+        console.log("Positive");
+    }else if(0>a){
+        console.log("Negative");
+    }else{
+        console.log("Zero");
+    }
+}
+posiNegaZero(-7);
