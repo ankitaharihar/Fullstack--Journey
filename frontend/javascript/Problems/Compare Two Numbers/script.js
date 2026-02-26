ae68c41d536d59a97445957e977056a89a195683
Ankita <ankitaharihar81@gmail.com>
2026-02-26 08:02:22 +0530

    Add implementation for Celsius to Fahrenheit Converter and Compare Two Numbers with HTML and JavaScript files

---
+++ frontend/javascript/Problems/Compare Two Numbers/script.js
@@ -0,0 +1,13 @@
+function compare(a,b){
+    let result
+    if(a>b){
+        console.log("a is greater");
+    }else if(b>a){
+        console.log("b is greater")
+
+    }else{
+        console.log("Both are equal");
+    }
+    return result;
+}
+compare(6,6);
