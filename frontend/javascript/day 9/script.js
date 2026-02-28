e7bbe1bbaebccfde419b475299174c4503aeb9dc
Ankita <ankitaharihar81@gmail.com>
2026-02-28 11:51:21 +0530

    Add HTML and JavaScript files demonstrating array operations and methods

---
+++ frontend/javascript/day 9/script.js
@@ -0,0 +1,46 @@
+// let cities = ["Pune","Nashik","Banglore","Hyderabad","Chennai","Delhi","Kolkata"];
+
+// console.log(cities.length);
+
+
+// console.log(cities.at(-1));
+
+
+//push adds element to the end of array
+// cities.push("Mumbai");
+// console.log(cities);
+
+
+//pop removes element from the end of array
+// cities.pop();
+// console.log(cities);
+
+
+//shift removes element from the beginning of array
+// cities.shift();
+// console.log(cities);
+
+//unshift adds element to the beginning of array
+// cities.unshift("Mumbai");
+// console.log(cities);
+
+
+//splice (index, count, element to add)
+// cities.splice(3,3,"Delhi");
+// console.log(cities);
+
+// reverse - reverses the order of elements in the array
+// cities.reverse();
+// console.log(cities);
+
+
+//isArray - checks if an object is an array
+// let cities1 = "Pune";
+// console.log(Array.isArray(cities1)); // false
+
+
+// let cities = {};
+// console.log(Array.isArray(cities)); // false
+
+// let cities2 = ["Pune","Nashik","Banglore","Hyderabad","Chennai","Delhi","Kolkata"];
+// console.log(Array.isArray(cities2)); // true
