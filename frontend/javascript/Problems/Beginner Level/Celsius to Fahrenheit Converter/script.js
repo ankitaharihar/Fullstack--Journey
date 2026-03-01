5e9b7e02bc67c0738452ad2b8ccbb1e1f3a1eed6
Ankita <ankitaharihar81@gmail.com>
2026-03-01 13:50:55 +0530

    Add Celsius to Fahrenheit conversion function and update console output

--- frontend/javascript/Problems/Beginner Level/Celsius to Fahrenheit Converter/script.js
+++ frontend/javascript/Problems/Beginner Level/Celsius to Fahrenheit Converter/script.js
@@ -1,3 +1,10 @@
 let Celsius = 9;
 let Fahrenheit = (Celsius * 9/5) + 32;
 console.log(Fahrenheit);
+
+
+// Write a function that takes a temperature in Celsius and returns the corresponding temperature in Fahrenheit.
+function celsiusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
+console.log(celsiusToFahrenheit(25));
