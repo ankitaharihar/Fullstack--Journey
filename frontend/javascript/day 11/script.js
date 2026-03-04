9f032531894c59175ada3f5fd8681c400a228581
Ankita <ankitaharihar81@gmail.com>
2026-03-04 12:48:10 +0530

    Added new terms

--- frontend/javascript/day 11/script.js
+++ frontend/javascript/day 11/script.js
@@ -4,3 +4,11 @@ function changeStyle(){
     // h1E1.classList.toggle("para");
     img.setAttribute("src","hehe.jpg");
 }
+
+const parent = document.querySelector(".parent");
+
+function createAndAddElement(){
+    const para = document.createElement("p");
+    para.innerText = "I am a new element";
+    parent.appendChild(para);
+}
