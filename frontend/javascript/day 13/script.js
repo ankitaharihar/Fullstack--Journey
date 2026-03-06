23deeb551bc65f34e347027c1b101bf85305893b
Ankita <ankitaharihar81@gmail.com>
2026-03-06 12:40:39 +0530

    Add initial HTML structure and JavaScript for user input handling

---
+++ frontend/javascript/day 13/script.js
@@ -0,0 +1,63 @@
+// //Function declaration
+// function add(a, b) {
+//     return a + b;
+// }   
+// console.log(add(2, 3));
+
+
+
+
+// //Function expression   
+// const subtract = function(a, b) {
+//     return a - b;
+// }
+// console.log(subtract(5, 2));
+
+
+
+
+// //Arrow function
+// const multiply = (a, b) => {
+//     return a * b;
+// }   
+// console.log(multiply(4, 6));
+
+
+
+// //arrow function
+// const btn = document.querySelector('button');
+
+// btn.addEventListener('click', (event) => {
+//     console.log('Button clicked!', event);
+// });
+
+// const btn = document.querySelector('button');
+
+// btn.removeEventListener('click', (event) => {
+//     console.log('Button clicked!', event);
+// });
+
+
+//window 
+// console.log(window);
+
+//offsetHeight
+// const element = document.querySelector('.my-element');
+// console.log(element.offsetHeight);
+//
+
+//create a elemet which accept name and print name in web
+
+
+
+// const nameInput =document.querySelector("input[type='text']");
+// const submitBtn=document.querySelector("button");
+// const h1 =document.querySelector("strong");
+// submitBtn.addEventListener('click', () => {
+//     h1.innerText = "Hi why are you coming again ?" + nameInput.value;
+
+// });
+//add multiple text in h1
+const nameInput =document.querySelector("input[type='text']");
+const submitBtn=document.querySelector("button");
+const h1 =document.querySelector("strong");
