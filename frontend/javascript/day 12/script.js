959ce8e9a970fbf2c525215404f6aeb5f5291bef
Ankita <ankitaharihar81@gmail.com>
2026-03-06 11:12:40 +0530

    Add initial HTML and JavaScript files for event handling example

---
+++ frontend/javascript/day 12/script.js
@@ -0,0 +1,13 @@
+// select button 
+const btn = document.querySelector("button");
+
+// way handling and only write refrence
+//btn.onclick = dosomething;
+
+
+// 3rd way 
+// btn.addEventListener("click", dosomething);
+
+function dosomething(){
+    console.log("Doing something");
+}
