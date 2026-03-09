048172b358d4bb8b0aea64cae377a98e29d7f0a4
Ankita <ankitaharihar81@gmail.com>
2026-03-09 16:37:57 +0530

    Add HTML and JavaScript files for Array methods demonstration

---
+++ frontend/javascript/day 15/script.js
@@ -0,0 +1,23 @@
+// Find Maximum Number in an Array
+// let arr = [3,7,2,9,5];
+// let max = arr[0];
+// for(let i=1; i<arr.length;i++){
+//     if(arr[i] > max){
+//         max = arr[i];
+//     }
+// }
+// console.log(max);
+
+
+// Find Minimum number in an array 
+
+let arr = [3,7,2,9,5];
+let min = arr[0];
+for(let i = 1 ; i <arr.length;i++){
+    if(arr[i]< min){
+        min = arr[i]
+    }
+}
+console.log(min);
+
+
