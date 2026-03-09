9e06cf7d5a798fe653bdf24d3b8f06811c07162d
Ankita <ankitaharihar81@gmail.com>
2026-03-09 07:06:14 +0530

    Add HTML and JavaScript files for JS Strings and string manipulation examples

---
+++ frontend/javascript/day 14/script.js
@@ -0,0 +1,79 @@
+// let city1 = "Pune";
+// let city2 = 'Pune';
+// let city3 = `Pune`;
+
+// let sentence = city1 + city2 + city3
+// console.log(sentence)
+
+
+
+// let city = "Pune";
+// let userName="Ani";
+
+// let sentence = `My name is ${userName} and i live in ${city}.`
+
+// console.log(sentence);
+
+
+// let city = "Pune";
+// console.log(city[1]);
+// //toUpperCase()
+// //toLowerCase()
+
+// console.log(city[-1]);
+
+// //at
+// console.log(city.at(-1));
+
+//substring   | slice 
+// console.log(city.substring(1,4));
+// console.log(city.slice(1,4));
+
+//  Realworld Example of substring() and slice()
+   
+// let email = "john.doe45@example.com"
+// let username = email.substring(0, email.indexOf('@'));
+// console.log(username); // Output: "john.doe45"
+
+//trim()   | trimStart()  | trimEnd()
+
+//  let userName = "   John Doe   ";
+//  console.log(userName.trim()); // Output: "John Doe"
+//  console.log(userName.trimStart()); // Output: "John Doe   "
+//  console.log(userName.trimEnd()); // Output: "   John Doe"
+
+
+//split() 
+
+// let sentence = "Hello World! Welcome to JavaScript.";
+
+// console.log(sentence.split(" ")); // Output: ["Hello", "World!", "Welcome", "to", "JavaScript."]
+
+//reverse the string 
+// let userName = "Ankita";
+// // let reversName= userName.split("").reverse();
+// // console.log(reversName.join(""));
+
+// //using for loop
+// let sum = " ";
+
+// for(let i = userName.length-1 ; i >= 0 ; i--){
+//     sum = sum + userName[i];
+// }
+// console.log(sum);
+
+// replace 
+// include
+//endsWith()
+//startWith()
+
+
+function getcapital(str){
+    let trim1  = string.trim();
+    let trim2 = trim1.toLowerCase()
+ let newStr = trim2[0].toUppercase().trim2.slice(1);
+ return 
+}
+
+
+console.log("      pune");
