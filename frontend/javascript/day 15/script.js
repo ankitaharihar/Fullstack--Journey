04d08e1ab8f64d1818e7118310493d1c5c2026c2
Ankita <ankitaharihar81@gmail.com>
2026-03-10 11:49:11 +0530

    Comment out code for finding the minimum number in an array

--- frontend/javascript/day 15/script.js
+++ frontend/javascript/day 15/script.js
@@ -11,13 +11,13 @@
 
 // Find Minimum number in an array 
 
-let arr = [3,7,2,9,5];
-let min = arr[0];
-for(let i = 1 ; i <arr.length;i++){
-    if(arr[i]< min){
-        min = arr[i]
-    }
-}
-console.log(min);
+// let arr = [3,7,2,9,5];
+// let min = arr[0];
+// for(let i = 1 ; i <arr.length;i++){
+//     if(arr[i]< min){
+//         min = arr[i]
+//     }
+// }
+// console.log(min);
 
 
