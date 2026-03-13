676e9030614244303677e30527c2c7c610252729
Ankita <ankitaharihar81@gmail.com>
2026-03-13 06:41:28 +0530

    Add initial HTML, JavaScript, and script files for day 19 exercises on destructuring and exports

---
+++ frontend/javascript/day 19/main.js
@@ -0,0 +1,6 @@
+//Normal export
+// export let name = "Rsni"
+//let age = 89;
+
+//export default you can use only on in one file 
+// export default age;
---
+++ frontend/javascript/day 19/script.js
@@ -0,0 +1,81 @@
+//Destructuring
+// to easily extract values from arrays and objects
+
+//Array des
+
+// let cities = ["Pune", "nashik", "banglore", "mumbai"];
+// let[ , , c] = cities;
+
+
+// function a(){
+//     return[10,20];
+// }
+
+// let [, , n3] = a();
+
+
+//create a variale the store persone info 
+// let person = {
+//     userName : "ANi",
+//     age : 99,
+//     city : "Pune"
+// }
+
+// let{age,userName} = person;
+// console.log(userName);
+
+
+
+//Spread op (...)
+// let cities = ["Pune", "nashik", "banglore", "mumbai"];
+// let cities2 = ["Pune", "nashik", "banglore", "mumbai"];
+
+// let neArr = [...cities, ...cities2];
+
+// //want to put new element
+
+// let neArrr = [...cities, "Delhi"];
+// console.log(neArr);
+
+
+
+//Merge two object using spread
+
+// let obj1= {
+//     userName:"Ani",
+//     age: 88
+// }
+
+// let obj2={
+//     city:"nashik",
+//     state:"Maha"
+
+// }
+
+// let neArr = {...obj1, ...obj2}
+
+
+
+
+//Rest operator write in function 
+
+// function add(...nums){
+//     console.log(nums);
+// }
+// add(13,33,44,55,67,68,89);
+
+
+
+
+//normal export in import
+// import{
+//     name
+// } from "./main.js"
+
+// //default export
+// import age from "./main.js";
+
+// console.log(name);
+
+
+
