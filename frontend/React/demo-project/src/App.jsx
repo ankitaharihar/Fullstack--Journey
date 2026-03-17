cbcf979c52bf87c7d9477ab985041045f281b77d
Ankita <ankitaharihar81@gmail.com>
2026-03-17 07:03:49 +0530

    feat: update App component to include login message and visibility toggle

--- frontend/React/demo-project/src/App.jsx
+++ frontend/React/demo-project/src/App.jsx
@@ -1,11 +1,17 @@
 import Firstdemo from './Firstdemo'
 function App() {
+  let isLoggedIn = true;
+  let isVisiable = false;
   return(
-    <div><h1>This is first demo project</h1>
-    <Firstdemo>
-      </Firstdemo>
-      <hr />
-      <TextCom text="my name is tux" city="Pune" age={56}></TextCom>
+    <div>
+      {isLoggedIn ? <p>Welcome Back</p> : <p>Login in</p>}
+      {isVisiable && <div>thiss is div </div>}
+    {MessageChannel.length>=100}
+
+      {/* <hr />
+      <TextCom text="my name is Ankita" city="Pune" age={56}></TextCom>
+      <TextCom text="I live in Pune"></TextCom>
+      <TextCom text="I love Video editing"></TextCom> */}
       </div>
   )
 }
