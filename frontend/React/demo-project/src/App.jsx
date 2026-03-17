4337711db00ff410e85426cdf443682c1289adde
Ankita <ankitaharihar81@gmail.com>
2026-03-17 13:59:46 +0530

    refactor: comment out unused variables and clean up TextCom props

--- frontend/React/demo-project/src/App.jsx
+++ frontend/React/demo-project/src/App.jsx
@@ -1,28 +1,30 @@
-import Firstdemo from './Firstdemo'
+// import Firstdemo from './Firstdemo'
 function App() {
-  let isLoggedIn = true;
-  let isVisiable = false;
+  // let isLoggedIn = true;
+  // let isVisiable = false;
+  
   return(
     <div>
-      {isLoggedIn ? <p>Welcome Back</p> : <p>Login in</p>}
+      {/* {isLoggedIn ? <p>Welcome Back</p> : <p>Login in</p>}
       {isVisiable && <div>thiss is div </div>}
-    {MessageChannel.length>=100}
+    {MessageChannel.length>=100} */}
 
-      {/* <hr />
+      <hr />
       <TextCom text="my name is Ankita" city="Pune" age={56}></TextCom>
-      <TextCom text="I live in Pune"></TextCom>
-      <TextCom text="I love Video editing"></TextCom> */}
+      <TextCom yy="I live in Pune"></TextCom>
+      <TextCom xx="I love Video editing"></TextCom>
       </div>
   )
 }
 
-function TextCom(props){
-  console.log("Something", props);
+function TextCom({text,yy, city}){
+  console.log("Something");
   return (
     <div>
-      <p>{props.text}</p>
-      <p>{props.city}</p>
-      <p>{props.age}</p>
+       
+      <p>{yy}</p>
+      <p>{text}</p>
+      <p>{city}</p>
     </div>
   )
 }
