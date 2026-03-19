7a362229127397c4eb966a349ed86c7d8da18784
Ankita <ankitaharihar81@gmail.com>
2026-03-19 23:57:33 +0530

    feat: add Header component with initial structure

---
+++ frontend/React/jor2/src/Header.jsx
@@ -0,0 +1,10 @@
+function Do(){
+    return(
+        <div className="container">
+            <h1>Heading</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.</p>
+        </div>
+    )
+}
+
+export default Do;
