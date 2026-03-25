34007d1b6348b4394900425e1f04b4fce4be592c
Ankita <ankitaharihar81@gmail.com>
2026-03-25 16:04:04 +0530

    feat: add NotFound component for 404 error handling

---
+++ frontend/React/react-tasks/src/Level2/Task 16/NotFound.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom"
+
+function NotFound() {
+  return (
+    <>
+      <h1>404 Page Not Found</h1>
+      <Link to="/">Go Home</Link>
+    </>
+  )
+}
+
+export default NotFound;
+
+
+//<Route path="*" element={<NotFound />} />
